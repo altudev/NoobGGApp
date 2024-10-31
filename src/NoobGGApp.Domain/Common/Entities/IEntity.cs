59edde38c82122fd81b110f75614e4486aafee81
ALPER TUNGA <alper@altu.dev>
2024-10-31 22:17:08 +0300

    [31/10/2024]: Solution and Domain project created.

---
+++ src/NoobGGApp.Domain/Common/Entities/IEntity.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace NoobGGApp.Domain.Common.Entities;
+
+public interface IEntity<TKey> where TKey : struct
+{
+    TKey Id { get; set; }
+}
---
+++ src/NoobGGApp.Domain/Entities/Game.cs
@@ -0,0 +1,31 @@
+using NoobGGApp.Domain.Common.Entities;
+using NoobGGApp.Domain.ValueObjects;
+
+namespace NoobGGApp.Domain.Entities;
+
+public sealed class Game : EntityBase<Guid>
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string ImageUrl { get; set; }
+
+
+    public Game()
+    {
+        Email userEmail = new Email("alper.tunga@yazilim.academy");
+
+        string email = userEmail;
+
+        Customer customer1 = new Customer("alper.tunga@yazilim.academy");
+
+        customer1.Address.City = "Ankara";
+
+        Customer customer2 = new Customer("alper.tunga@yazilim.academy");
+
+        if (customer1.Address == customer2.Address)
+        {
+
+        }
+    }
+
+}
